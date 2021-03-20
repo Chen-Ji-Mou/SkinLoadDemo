c5931d208aac2b335533941447474849cda82a33
陈垍谋 <1721554825@qq.com>
2021-03-20 21:21:12 +0800

    首次提交

---
+++ app/src/main/java/com/chenjimou/skinloaddemo/MyApplication.java
@@ -0,0 +1,11 @@
+package com.chenjimou.skinloaddemo;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
