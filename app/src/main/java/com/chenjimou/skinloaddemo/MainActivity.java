5dbfb044a294c0958822b5ef41568a2d6760f952
陈垍谋 <1721554825@qq.com>
2021-03-20 21:19:30 +0800

    首次提交

--- app/src/main/java/com/chenjimou/skinloaddemo/MainActivity.java
+++ app/src/main/java/com/chenjimou/skinloaddemo/MainActivity.java
@@ -3,6 +3,8 @@ package com.chenjimou.skinloaddemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener((v) -> {
+
+        });
+
+        Button button1 = findViewById(R.id.button2);
+        button1.setOnClickListener((v -> {
+
+        }));
     }
 }
