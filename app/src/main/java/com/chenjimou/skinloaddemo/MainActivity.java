00d13a4c53cd81600edf2e2cd34d647382ce8549
陈垍谋 <1721554825@qq.com>
2021-03-20 22:27:07 +0800

    首次提交

--- app/src/main/java/com/chenjimou/skinloaddemo/MainActivity.java
+++ app/src/main/java/com/chenjimou/skinloaddemo/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button button = findViewById(R.id.button);
         button.setOnClickListener((v) -> {
-            SkinManager.getInstance().loadSkin("");
+            SkinManager.getInstance().loadSkin("/storage/emulated/0/Android/data/com.tencent.mobileqq/Tencent/QQfile_recv/skin-debug(1).apk");
         });
 
         Button button1 = findViewById(R.id.button2);
